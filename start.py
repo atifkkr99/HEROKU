d24cb2c8038cc2aa275138abe4821962467337d9
Atif Sk <86765947+atifkkr99@users.noreply.github.com>
2021-07-31 16:33:06 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && cd ULTRA-X && python3 -m ULTRA")
+system ("git clone https://GitHub.com/atifkkr99/atifkkr5 && cd atifkkr5 && python3 -m ULTRA")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
